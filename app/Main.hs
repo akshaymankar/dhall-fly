5ed225f82aec8df7d54e136960a7f48c85384b58
Akshay Mankar <itsakshaymankar@gmail.com>
2019-04-07 00:56:33 -0400

    Move dhall->yaml translation logic out of main

--- app/Main.hs
+++ app/Main.hs
@@ -1,49 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import Dhall
-import Fly.Types
+import Data.Yaml     (encode)
+import Dhall         (auto, input)
+import Dhall.JSON    (omitNull)
 import Fly.Interpret
-import Data.Yaml
-import Dhall.JSON
-import Data.Maybe (catMaybes)
-import Data.List (nub)
+import Fly.Types     (Job)
+import Fly.Yaml      (dhallToYaml)
 
-import qualified Data.Text.IO
 import qualified Data.ByteString.Char8 as BC8
-
-customResourceTypes :: [ResourceType] -> [ResourceType]
-customResourceTypes [] = []
-customResourceTypes (ResourceTypeInBuilt _ : rts) =  customResourceTypes rts
-customResourceTypes (x : rts) =  x : customResourceTypes rts
-
-getResourcesFromHooks :: StepHooks -> [Resource]
-getResourcesFromHooks StepHooks{..} =
-  concatMap getResourcesFromStep
-  $ catMaybes [hookOnSuccess, hookOnFailure, hookOnAbort, hookEnsure]
-
-getResourcesFromStep :: Step -> [Resource]
-getResourcesFromStep s =
-  stepResources s ++ (getResourcesFromHooks $ Fly.Types.stepHooks s)
-  where
-    stepResources (Get (GetStep{..}) _) = [getResource]
-    stepResources (Put (PutStep{..}) _) = [putResource]
-    stepResources (Task _ _) = []
-    stepResources (Aggregate steps _) = concatMap getResourcesFromStep steps
-    stepResources (Do steps _) = concatMap getResourcesFromStep steps
-    stepResources (Try step _) = getResourcesFromStep step
-
-getResourcesFromJob :: Job -> [Resource]
-getResourcesFromJob Job{..} = concatMap getResourcesFromStep jobPlan
+import qualified Data.Text.IO          (getContents)
 
 main :: IO ()
 main = do
   stdin <- Data.Text.IO.getContents
   jobs <- (input auto stdin :: IO [Job])
-  let resources = nub $ concatMap getResourcesFromJob jobs
-      resourceTypes = nub $ customResourceTypes $ map Fly.Types.resourceType resources
-    in BC8.putStrLn $ encode $ omitNull $ object [ "resource_types" .= resourceTypes
-                                                 , "resources" .= resources
-                                                 , "jobs" .= jobs
-                                                 ]
+  BC8.putStrLn $ encode $ omitNull $ dhallToYaml jobs
---
+++ src/Fly/Yaml.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+module Fly.Yaml where
+
+import Data.Aeson
+import Data.Maybe (catMaybes)
+import Fly.Types
+import Data.List (nub)
+
+customResourceTypes :: [ResourceType] -> [ResourceType]
+customResourceTypes [] = []
+customResourceTypes (ResourceTypeInBuilt _ : rts) =  customResourceTypes rts
+customResourceTypes (x : rts) =  x : customResourceTypes rts
+
+getResourcesFromHooks :: StepHooks -> [Resource]
+getResourcesFromHooks StepHooks{..} =
+  concatMap getResourcesFromStep
+  $ catMaybes [hookOnSuccess, hookOnFailure, hookOnAbort, hookEnsure]
+
+getResourcesFromStep :: Step -> [Resource]
+getResourcesFromStep s =
+  stepResources s ++ (getResourcesFromHooks $ Fly.Types.stepHooks s)
+  where
+    stepResources (Get (GetStep{..}) _) = [getResource]
+    stepResources (Put (PutStep{..}) _) = [putResource]
+    stepResources (Task _ _)            = []
+    stepResources (Aggregate steps _)   = concatMap getResourcesFromStep steps
+    stepResources (Do steps _)          = concatMap getResourcesFromStep steps
+    stepResources (Try step _)          = getResourcesFromStep step
+
+getResourcesFromJob :: Job -> [Resource]
+getResourcesFromJob Job{..} = concatMap getResourcesFromStep jobPlan
+
+dhallToYaml :: [Job] -> Value
+dhallToYaml jobs =
+  let resources = nub $ concatMap getResourcesFromJob jobs
+      resourceTypes = nub $ customResourceTypes $ map Fly.Types.resourceType resources
+  in object [ "resource_types" .= resourceTypes
+            , "resources" .= resources
+            , "jobs" .= jobs
+            ]
