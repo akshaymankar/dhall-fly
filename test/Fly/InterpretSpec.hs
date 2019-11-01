5ee55a2f229f49c68cb96c1aa1a36dbc86dc0616
Akshay Mankar <itsakshaymankar@gmail.com>
2019-11-01 14:57:37 +0000

    Fix tests

--- test/Fly/InterpretSpec.hs
+++ test/Fly/InterpretSpec.hs
@@ -15,15 +15,16 @@ spec = do
   describe "Fly.Interpret" $ do
     describe "Interpret ResourceType" $ do
       it "should interpret in built resource type" $ do
-        resourceType <- input auto "let x = ./dhall-concourse/examples/kubo/resources/git-kubo-ci.dhall in x.type"
+        resourceType <- input auto "./test/data/in-built-resource-type.dhall"
         resourceType `shouldBe` ResourceTypeInBuilt "git"
+
       it "should interpret custom resource type" $ do
-        resourceType <- input auto "./dhall-concourse/examples/kubo/resource-types/bosh-deployment.dhall"
+        resourceType <- input auto "./test/data/custom-resource-type.dhall"
         resourceType `shouldBe` boshDeploymentResourceType
 
     describe "Interpret Resource" $ do
       it "should interpret a git resource" $ do
-        resource <- input auto "./dhall-concourse/examples/kubo/resources/git-kubo-ci.dhall"
+        resource <- input auto "./test/data/resource.dhall"
         resource `shouldBe` gitKuboCI
 
     describe "Interpret TaskConfig" $ do
@@ -31,25 +32,32 @@ spec = do
         task <- input auto "./dhall-concourse/defaults/TaskConfig.dhall"
         task `shouldBe` defaultTaskConfig
 
-boshDeploymentResourceType = ResourceTypeCustom{..} where
-  rtcName = "bosh-deployment"
-  rtcType = "docker-image"
-  rtcSource =  pure $ fromList [ ("repository", String "cloudfoundry/bosh-deployment-resource")
+boshDeploymentResourceType = ResourceTypeCustom (CustomResourceType{..}) where
+  crtName = "bosh-deployment"
+  crtType = "docker-image"
+  crtSource =  pure $ fromList [ ("repository", String "cloudfoundry/bosh-deployment-resource")
                                , ("tag", String "latest")
                                ]
+  crtPrivileged = Nothing
+  crtParams = Nothing
+  crtCheckEvery = Nothing
+  crtTags = Nothing
+  crtUniqueVersionHistory = Nothing
 
 gitKuboCI = Resource{..} where
   resourceName =  "git-kubo-ci"
   resourceType = ResourceTypeInBuilt "git"
   resourceCheckEvery = Nothing
   resourceParams = Nothing
-  resourceSource = Just $ object [ "privateKey" .= "((git-ssh-key.private_key))"
-                                 , "uri" .= "git@github.com:cloudfoundry-incubator/kubo-ci"
-                                 , "branch" .= "master"
-                                 ]
+  resourceSource = Just $ fromList [ "privateKey" .= "((git-ssh-key.private_key))"
+                                   , "uri" .= "git@github.com:cloudfoundry-incubator/kubo-ci"
+                                   , "branch" .= "master"
+                                   ]
   resourceTags = Nothing
   resourceVersion = Nothing
   resourceWebhookToken = Nothing
+  resourceIcon = Nothing
+  resourcePublic = Nothing
 
 defaultTaskRunConfig = TaskRunConfig{..} where
   trcPath = "CHANGEME"
@@ -65,4 +73,5 @@ defaultTaskConfig = TaskConfig{..} where
   tcInputs = Nothing
   tcOutputs = Nothing
   tcParams = Nothing
-  tcRootfsURI = Nothing
+  tcRootfsUri = Nothing
+  tcContainerLimits = Nothing
