984ea12d6bfee8ff67b93e5b243045a57ae790e5
Akshay Mankar <itsakshaymankar@gmail.com>
2019-11-02 21:15:53 +0000

    Replace yaml with aeson-yaml
    It doesn't depend on C binding and it is already dependant of dhall-json

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.Yaml     (encode)
-import Dhall         (auto, input)
-import Dhall.JSON    (omitNull)
-import Fly.Types     (Job)
-import Fly.Yaml      (dhallToYaml)
+import Data.Aeson.Yaml (encode)
+import Dhall           (auto, input)
+import Dhall.JSON      (omitNull)
+import Fly.Types       (Job)
+import Fly.Yaml        (dhallToYaml)
 
-import qualified Data.ByteString.Char8 as BC8
-import qualified Data.Text.IO          (getContents)
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.Text.IO               (getContents)
 
 main :: IO ()
 main = do
   stdin <- Data.Text.IO.getContents
-  jobs <- (input auto stdin :: IO [Job])
-  BC8.putStrLn $ encode $ omitNull $ dhallToYaml jobs
+  jobs <- input auto stdin :: IO [Job]
+  LBS.putStrLn $ encode $ omitNull $ dhallToYaml jobs
